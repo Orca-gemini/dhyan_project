0961a09998d1c8ae76d675e4522c1b6bdb3fce73
Orca-gemini <orcagemini3@gmail.com>
2023-03-22 11:55:27 +0530

    Added a calendar

---
+++ project/mainjs.js
@@ -0,0 +1,20 @@
+let calendar = document.querySelector('.calendar')
+// used let instead of var because it's more block scoped !! 
+// query selector returns the first element that matches a css selector 
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
+}
+
+getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28
+}
+
+calendar_gen = (month, year) => {
+    let cal_days = calendar.querySelector('.caledar-days')
+    let cal_header_year = calendar.querySelector('#year')
+
+    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+}
